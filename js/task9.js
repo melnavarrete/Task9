e9aedaeb8e2f7cf10b670fb02b8c2072f0277e36
Mel Navarrete <melnavarrete@gmail.com>
2022-11-27 21:57:35 -0500

    push assignment 9

---
+++ js/task9.js
@@ -0,0 +1,75 @@
+Melanie Navarrete
+CSC 225
+
+//Task 1 array backwards
+bckwrds = 'Backwards'
+function back(){
+    function(bckwrds);
+    for(let i = this.length; i>=0; i--){
+        console.log(str[i]);
+    }
+}
+
+
+//Task 2 takes the input string makes all lowercase
+const lrcase = new array ('ThInGs');
+console.log (lrcase);
+
+//Task 3 converts from farenheit to celcius
+
+function tempto(temp){
+    console.log(temp-32)*(5/9);
+}
+
+//Task 4 Takes an input array and returns booleans >=75 T or F
+bool=tf;
+int[nums] = {15; 32; 200};
+    if (nums >=75){
+        bool=true;
+        console.log ('T');
+    }
+    else{
+        bool=false;
+        console.log ('F');
+    }
+}
+return tf;
+
+
+
+//Loops through 2 variables and returns an array
+str bands ['Corn', 'Cake' ];
+for (var x=0; x<3; x++){
+    bands.push(bands);
+}
+console.log[bands];
+
+
+//Returns an array of only prime numbers
+
+int nums = new nums[1,2,3,4,5,6,7,8,9];
+for (int x=9; x<nums[i]; i++){
+if(nums  /2 % !=  0){
+    console.log num;
+    }
+else{
+    nums.remove(i);
+}
+}
+
+
+
+//loops through console log and exact multiple of 3 "225 Rules!" no number 5  I love Javascript and 3 and 5 CSC225 rules I love JavaScript
+for(i=1;i<=100; i++){
+    if (i%3 && i % 5 = 0) {
+        console.log('CSC225 Rules I love Javascript');
+    }
+    else if(i % 3 && i % 5 != 0){
+        console.log('CSC 225 Rules');
+     } else if( i % 5 = 0 && i % 3 != 0){
+        console.log('I love JavaScript';);
+     }
+     else{
+        console.log(i);
+     }    
+}
